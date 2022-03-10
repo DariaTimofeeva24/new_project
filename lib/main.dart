7ed5e52b444e3c70b805156ea643a5b847e955b1
Daria Kuzina <d.k.24@yandex.ru>
2022-03-10 09:40:44 +0300

    networking

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:new_project/shared_pref.dart';
 import 'package:new_project/switch.dart';
 import 'package:new_project/theme_demo.dart';
 
 import 'appbar.dart';
+import 'listview.dart';
 import 'navigation.dart';
+import 'networking.dart';
 
 
 
 void main() {
-  runApp(const ThemesDemoApp());
+  runApp(const MyApp());
 }
